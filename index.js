75c38c09aa1ac367ca19bdbee986bcdf98ebd1dc
Harikrishnan  Areekara <harikrishnan.areekara@gapblue.com>
2025-01-28 18:21:01 +0530

    added .env

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ app.use(express.json());
 const Product = require("./models/product.models.js");
 app.use(express.urlencoded({ extended: false }));
 const productRoutes = require("./routes/products.routes");
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use("/api/products", productRoutes);
 
 mongoose
-  .connect(
-    "mongodb+srv://harimongo:SkOGeZp4kxvJbX62@backenddb.yazkg.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected!");
   })
